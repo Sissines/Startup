a16107198e119de97be0d3a7384e399ef191e88f
Sissines <92338885+Sissines@users.noreply.github.com>
2022-02-10 11:49:33 -0500

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt full-upgrade -y
+sudo apt dist-upgrade -y
+sudo apt install apt-utils -y
+sudo apt install i2c-tools -y
+sudo apt-get install gcc-arm*
+sudo apt install libi2c-dev -y
+sudo apt install git -y
+sudo apt install python3-dev -y
+sudo apt install python3 -y
+sudo apt install python3-pip -y 
+sudo apt install python3-serial -y
+sudo apt install python3-psutil -y 
+sudo apt install python3-pil -y
+sudo pip3 install luma.oled 
