67b0117682f289f6cbf8c2ceb049fbe3c617e854
Sissines <92338885+Sissines@users.noreply.github.com>
2022-02-17 16:30:51 -0500

    Update adafruit.sh

--- adafruit.sh
+++ adafruit.sh
@@ -3,3 +3,6 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt full-upgrade -y
 sudo apt dist-upgrade -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-pil -y
+sudo pip3 install adafruit-circuitpython-ssd1306 -y
