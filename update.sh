dd66da6c7ed5c0ad0bde5fd67eb27e3434c9ecab
Sissines <92338885+Sissines@users.noreply.github.com>
2022-02-22 14:34:31 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
-echo "Updating"
+clear
+echo "$(tput setaf 3)     Updating"
 sudo apt update -y
 clear
-echo "Update Complete"
-echo "Upgrading"
+echo "$(tput setaf 2)     Update Complete"
+echo "$(tput setaf 3)     Upgrading"
 sudo apt upgrade -y
 clear
-echo "Update Complete"
+echo "$(tput setaf 2)     Update Complete"
 echo "Upgrade Complete"
-echo "Full-Upgrade Started"
+echo "$(tput setaf 3)     Full-Upgrade Started"
 sudo apt full-upgrade -y
 clear
-echo "Update Complete"
-echo "Upgrade Complete"
-echo "Full-Upgrade Complete"
-echo "Distro Upgrading"
+echo "$(tput setaf 2)     Update Complete"
+echo "$(tput setaf 2)     Upgrade Complete"
+echo "$(tput setaf 2)     Full-Upgrade Complete"
+echo "$(tput setaf 3)     Distro Upgrading"
 sudo apt dist-upgrade -y
 clear
-echo "Update Complete"
-echo "Upgrade Complete"
-echo "Full-Upgrade Complete"
-echo "Distro Upgraded"
-echo "Cleaning File System"
+echo "$(tput setaf 2)     Update Complete"
+echo "$(tput setaf 2)     Upgrade Complete"
+echo "$(tput setaf 2)     Full-Upgrade Complete"
+echo "$(tput setaf 2)     Distro Upgraded"
+echo "$(tput setaf 3)     Cleaning File System"
 sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
 clear
-echo "Update Complete"
-echo "Upgrade Complete"
-echo "Full-Upgrade Complete"
-echo "Distro Upgraded"
-echo "File System Cleaned"
+echo "$(tput setaf 2)     Update Complete"
+echo "$(tput setaf 2)     Upgrade Complete"
+echo "$(tput setaf 2)     Full-Upgrade Complete"
+echo "$(tput setaf 2)     Distro Upgraded"
+echo "$(tput setaf 2)     File System Cleaned"
